2f6e54a2d3ad4cefa108e229abc3a14ea820848a
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-13 14:41:47 +0100

    Refactor mywc program to improve readability and add missing line breaks
    This commit refactors the mywc program to improve readability by adding line breaks and indentation. Additionally, missing line breaks were added to separate the different sections of the program. These changes enhance the code's maintainability and make it easier to understand.

--- TP4/exercice4.c
+++ TP4/exercice4.c
@@ -1,6 +1,8 @@
 /* Exercice 4 — Recodons les utilitaires Unix
-Écrivez un programme mywc (qui est une version semblable au wc du Shell), qui compte les lignes, les mots et les caractères d’un fichier, et écrit les résultats sur la sortie standard. Un mot est une suite de caractères quelconques, encadré
-par un ou plusieurs espaces.
+Écrivez un programme mywc (qui est une version semblable au wc du Shell), qui compte les lignes, 
+les mots et les caractères d’un fichier, et écrit les résultats sur la sortie standard. 
+
+Un mot est une suite de caractères quelconques, encadré par un ou plusieurs espaces.
 On fera attention d’avoir un comportement similaire au véritable wc. Les fichiers pourront être lus soit depuis l’entrée
 standard soit donnés en paramètre. Dans les deux cas l’affichage doit être le même que celui la commande d’origine. On
 implémentera aussi les trois options :
