4bae96a80ea84510faf03d4e1ec13a37b36ba743
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-13 14:41:15 +0100

    Remove duplicate test_supprimer function

---
+++ TP4/exercice4.c
@@ -0,0 +1,9 @@
+/* Exercice 4 — Recodons les utilitaires Unix
+Écrivez un programme mywc (qui est une version semblable au wc du Shell), qui compte les lignes, les mots et les caractères d’un fichier, et écrit les résultats sur la sortie standard. Un mot est une suite de caractères quelconques, encadré
+par un ou plusieurs espaces.
+On fera attention d’avoir un comportement similaire au véritable wc. Les fichiers pourront être lus soit depuis l’entrée
+standard soit donnés en paramètre. Dans les deux cas l’affichage doit être le même que celui la commande d’origine. On
+implémentera aussi les trois options :
+1. -c (ou --bytes),
+2. -l (ou --lines),
+3. -w (ou --words),*/
