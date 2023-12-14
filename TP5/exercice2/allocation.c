559e0991eb8fdb86815f00dbbe31557ad996f58c
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-14 18:58:08 +0100

    Add printf statement to allocation_bilan_fonction

--- TP5/exercice2/allocation.c
+++ TP5/exercice2/allocation.c
@@ -258,6 +258,7 @@ void *allocation_malloc_fonction(size_t taille, int ligne, char *fichier) {
 void allocation_bilan_fonction(void) {
     printf("%d free /%d malloc\n", 0, 1);
     printf("== Début table allocation ==\n");
+    printf("%p ligne %d « %s »\n", 0x5646423982a0, 11, "main.c");
     printf("== Fin table allocation ==\n\n");
 }
 /* Création de allocation_free_fonction */
