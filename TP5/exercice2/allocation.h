83d6615a756d40861e1ad395af79f4d290429100
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-14 18:19:12 +0100

    Add allocation macros

---
+++ TP5/exercice2/allocation.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define allocation_malloc(X) allocation_malloc_fonction(X, __LINE__, __FILE__);
+#define allocation_bilan(X) allocation_bilan_fonction(__LINE__, __FILE__);
+#define allocation_free(X) allocation_free_fonction(X, __LINE__, __FILE__);
+
