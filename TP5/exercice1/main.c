3ef94feddc7bffeac87ca673e0b3b9a26f4703b4
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-14 15:55:48 +0100

    Refactor variable names for clarity and consistency
    This commit refactors variable names in the codebase to improve clarity and maintain consistency. The changes are based on best practices and make the code easier to understand and maintain.

---
+++ TP5/exercice1/main.c
@@ -0,0 +1,5 @@
+/* Partie i : Préparation du répertoire
+1. Dans le répertoire tp5, veillez créer un nouveau repertoire exercice1/. Créez un fichier main.c se contetant
+d’afficher « Hello World ! » et un fichier Makefile comme vue dans le cours 5. Une fois que tout marche bien
+passez à la question suivante. */
+
