84dfe5212bc9e0fc535fea1acc6a9d60cb3e17cc
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-13 21:00:45 +0100

    Add test_q1 function and implement fusion function

--- Annales/examen.c
+++ Annales/examen.c
@@ -13,9 +13,27 @@ int nombre_chiffres(char *chaine) {
     return nb_chiffres;
 }
 
+void test_q1(void) {
+    char *chaine = "abc 45+";
+    printf("Il y a %d chiffres dans la chaine %s\n", nombre_chiffres(chaine), chaine);
+}
+
+/* 2. Écrire une procèdure fusion qui prend en argument un entier n et trois tableaux, t1, t2 et t3 de taille n et qui
+initialise t3 en affectant chaque t3[i] (avec i<n) à la plus grande des valeurs entre t1[i] et t2[i]. En prenant,
+par exemple, n=3, t1 = 12 5 13 et t2 = 20 2 13 , alors t3 devra être initialisé à 20 5 13 . */
+
+void fusion(int n, int t1[], int t2[], int t3[]) {
+    for (int i = 0; i < n; i++) {
+        if (t1[i] > t2[i]) {
+            t3[i] = t1[i];
+        } else {
+            t3[i] = t2[i];
+        }
+    }
+}
+
 int main(void) {
-    printf("Question 1\n");
-    char chaine[] = "abc 45+";
-    printf("%d\n", nombre_chiffres(chaine));
+    print("Question 1\n");
+    test_q1();
     return 0;
 }
